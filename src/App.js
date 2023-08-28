35695a984c603238e6eb76f6df5e2e79b27e8c66
AssanRabari <assandesai@gmail.com>
2023-08-28 14:44:18 +0530

    error message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { useState } from "react";
 function App() {
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const renderErrorMessage = (name) => {
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.name}</div>
+    )
+  }
+
   
   return (
     <div className="App">
