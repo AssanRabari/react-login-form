a26168ecc437cce69a7c5733605fd7dee33a45f4
AssanRabari <assandesai@gmail.com>
2023-08-28 15:07:55 +0530

    handle submit verify user logic

--- src/App.js
+++ src/App.js
@@ -30,27 +30,48 @@ function App() {
     pass: "Invalid password"
   };
   const handleSubmit = (event) => {
-
     event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    const userData = users.find((user) => user.username === uname.value);
+
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
   }
 
-  return (
+  const renderform = (
     <div className="form">
       <form onSubmit={handleSubmit}>
-          <div className="input-container">
-            <label>Username</label>
-            <input type="text" name="uname" required />
-            {renderErrorMessage("uname")}
-          </div>
-          <div className="input-container">
-            <label>Password</label>
-            <input type="password" name="pass" required />
-            {renderErrorMessage("pass")}
-          </div>
-          <div className="button-container">
-              <input type="submit" />
-          </div>
+        <div className="input-container">
+          <label>Username</label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password</label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
       </form>
+    </div>
+  );
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderform}
+      </div>
     </div>
   );
 }
