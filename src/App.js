98590f028d1bfed5d0e2e01bd4465103e8a7dfcc
AssanRabari <assandesai@gmail.com>
2023-08-28 14:49:14 +0530

    login form using jsx

--- src/App.js
+++ src/App.js
@@ -10,10 +10,24 @@ function App() {
     )
   }
 
-  
+
   return (
-    <div className="App">
-      Login form
+    <div className="form">
+      <form>
+          <div className="input-container">
+            <label>Username</label>
+            <input type="text" name="uname" required />
+            {renderErrorMessage("uname")}
+          </div>
+          <div className="input-container">
+            <label>Password</label>
+            <input type="password" name="pass" required />
+            {renderErrorMessage("pass")}
+          </div>
+          <div className="button-container">
+              <input type="submit" />
+          </div>
+      </form>
     </div>
   );
 }
